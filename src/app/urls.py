9b046df9ac639c897dabe7a5e74ca2e771ff21f3
sieky <geromesiekytounkara@gmail.com>
2023-07-25 23:36:59 +0000

    Product Category views models controllers

--- src/app/urls.py
+++ src/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home-page"),
+    path('category/<slug:val>', views.CategoryView.as_view(), name="category"),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,4 +1,12 @@
+from urllib import request
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+
 
 def home(request):
     return render(request, "app/home.html")
+
+class CategoryView(View):
+    def get(self,request,val):
+        return render(request, "app/category.html",locals())
