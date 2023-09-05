0f1b236ebece3cd9f828c6042bf4b09e83c43cde
sieky <geromesiekytounkara@gmail.com>
2023-09-05 15:39:07 +0000

    User password, Reset Password, Confirm password

--- src/app/models.py
+++ src/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,6 +11,12 @@ CATEGORY_CHOICES =(
     ('CO', 'Céréales et œufs'),
     ('BS', 'Boissons'),
     ('PE', 'Produits d\'épicerie'),
+)
+STATE_CHOICES = (
+    ('basse guinee', 'Basse Guinée'),
+    ('moyenne guinee', 'Moyenne Guinée'),
+    ('haute guinee', 'Haute Guinée'),
+    ('guinee n\'zerekore', 'Guinée N\'Zérékoré')
 )
 
 class Product(models.Model):
@@ -23,3 +30,14 @@ class Product(models.Model):
     product_image = models.ImageField(upload_to='product')
     def __str__(self):
         return self.title
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+    def __str__(self):
+        return self.name
